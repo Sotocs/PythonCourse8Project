44e0401d15b41f801629af2c7a624372b54a9046
Nick <Nickolay.Klochkov@mail.ru>
2025-12-21 11:40:09 +0300

    complete project widget masks

--- scr/masks.py
+++ scr/masks.py
@@ -1,4 +1,4 @@
-from typing import Union, Any, Optional
+from typing import Union
 
 
 def get_mask_card_number(number: Union[str, int]) -> str:
@@ -6,7 +6,8 @@ def get_mask_card_number(number: Union[str, int]) -> str:
     Маскирует номер карты: первые 6 цифр видны, с 7‑й по 12‑ю — звёздочки, последние 4 — видны.
     Формат: 1234 56** **** 3456
     """
-    return str(number)[:4] + " " + str(number)[4:6] + "** **** " + str(number)[-4:]
+    number = str(number)
+    return str(number)[-16:-12] + " " + str(number)[-12:-14] + "** **** " + str(number)[-4:]
 
 
 def get_mask_account(number: Union[str, int]) -> str:
@@ -14,5 +15,4 @@ def get_mask_account(number: Union[str, int]) -> str:
     Маскирует номер счёта: все цифры, кроме последних 4, заменяются на звёздочки.
     Формат: (**** 1234).
     """
-    return "** " + str(number)[-4:]
-
+    return "**** " + str(number)[-4:]
---
+++ scr/widget.py
@@ -0,0 +1,33 @@
+from scr.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(name_number: str) -> str:
+    """Принимает строку с содержанием имени: карта или счет
+    Maestro 1596837868705199
+    Счет 64686473678894779589
+    Выводит замаскированную строку с маской в зависимости от выбора Счет или Карта"""
+    if name_number[:4] == "Счет":
+
+        return str(get_mask_account(name_number))
+    else:
+        return str(get_mask_card_number(name_number))
+
+
+# #for e in ['Maestro 1596837868705199',
+#           'Счет 64686473678894779589',
+#           'MasterCard 7158300734726758',
+#           'Счет 35383033474447895560',
+#           'Visa Classic 6831982476737658',
+#           'Visa Platinum 8990922113665229',
+#           'Visa Gold 5999414228426353',
+#           'Счет 73654108430135874305']:
+#     print(mask_account_card(e))
+
+
+def get_date(date: str) -> str:
+    """Принимает троку формата: 2024-03-11T02:26:18.671407
+    И возвращает строку формата:  "ДД.ММ.ГГГГ"("11.03.2024")."""
+    return date[8:10] + "." + date[5:7] + "." + date[:4]
+
+
+# print(get_date('2024-03-11T02:26:18.671407'))
