e05da9e727d5c16539b97f6f3578babf3ad87213
Nick <Nickolay.Klochkov@mail.ru>
2026-03-03 22:18:12 +0300

    Tests 50% complete

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,10 @@
 import pytest
 from scr import masks, widget
 
+
 def test_get_mask_card_number():
     assert masks.get_mask_card_number(1234564323133456) == '1234 56** **** 3456'
-    assert masks.get_mask_card_number('1234 5643 2313 3456')  == '1234 56** **** 3456'
+    assert masks.get_mask_card_number('1234 5643 2313 3456') == '1234 56** **** 3456'
     with pytest.raises(ValueError) as exc_info:
         masks.get_mask_card_number('1234 5643 2313 34564')
         print(exc_info)
@@ -17,6 +18,7 @@ def test_get_mask_card_number():
         masks.get_mask_card_number(123231677)
         print(exc_info)
 
+
 def test_get_mask_account():
     assert masks.get_mask_account('1234 4344 3333 3456') == '**** 3456'
     assert masks.get_mask_account('1234 43443333 3456') == '**** 3456'
@@ -24,6 +26,7 @@ def test_get_mask_account():
         masks.get_mask_account('1234222243443333 3456')
     with pytest.raises(ValueError) as exc_info:
         masks.get_mask_account('123456')
+
 
 def test_mask_account_card():
     assert widget.mask_account_card('Счет 1234564323133456') == '**** 3456'
@@ -40,10 +43,9 @@ def test_mask_account_card():
     #           'Счет 73654108430135874305']:
     #     print(mask_account_card(e))
 
+
 def test_get_date():
     assert widget.get_date('2024-03-11T02:26:18.671407') == '11.03.2024'
     assert widget.get_date('2025-04-20T02:26:18.671407') == '20.04.2025'
     with pytest.raises(ValueError) as exc_info:
         widget.get_date('2025-04-T02:26:18.671407')
-
-
